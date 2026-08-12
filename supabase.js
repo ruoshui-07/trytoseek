8a68850e21bc68ec9d55cdcaea5505823de4af1c
ruoshui-07 <1807198366@qq.com>
2026-08-13 00:14:05 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
 "https://verrithqsanxnhrtwjps.supabase.co";
 
 const SUPABASE_ANON_KEY =
-"你的anon key";
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlcnJpdGhxc2FueG5ocnR3anBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY0OTc4MjMsImV4cCI6MjEwMjA3MzgyM30.pm8PZKgNyhQsExsu4vHT3BdbgP8bME8iA7nO8BXvle0";
 
 
 const supabaseClient =
